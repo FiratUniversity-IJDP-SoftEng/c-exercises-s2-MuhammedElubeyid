3a2f3ac5a4cbc836124e42374d7172900a9c2571
MuhammedElubeyid <eprrmkz12345@gmail.com>
2024-11-03 21:49:06 +0300

    Update ex06.c

--- ex06.c
+++ ex06.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
 
+int num(void);
+
+int main(void){
+
+int numbers[num()];
+    int j=0;
+    for(int i= 100; i>=50; i--){
+    if(i%3 ==0){
+     numbers[j]=i;
+      printf("%d,",numbers[j]);
+     j++;
+       }
+  }
+
+}
+
+int num(void){
+int length= 0;
+for(int i= 100; i>=50; i--){
+    if(i%3 ==0){
+        length++;
+    }
+}
+  return length;
+}
