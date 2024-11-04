7c7611a95ea9f0e3270fd99454e836440a58e720
MuhammedElubeyid <eprrmkz12345@gmail.com>
2024-11-04 14:57:57 +0300

    Update ex09.c

--- ex09.c
+++ ex09.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
+#include <cs50.h>
 
+int main (void){
+
+    int numbers= 0;
+     int i= 0;
+     int sum = 0;
+    while(numbers != -1){
+      sum += numbers;
+      i++;
+      numbers= get_int("Enter the %d. number: ",i);
+     // sum += numbers;
+      // i++;
+    }
+    i--;
+      printf("Sum: %d\n",sum);
+      printf("Average: %f\n",sum/ (float)i);
+      printf("%d",i);
+
+}
