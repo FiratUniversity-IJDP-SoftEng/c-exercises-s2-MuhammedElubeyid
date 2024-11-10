9bd15eeac22810fb81ed2ff70cd06d347518f252
MuhammedElubeyid <eprrmkz12345@gmail.com>
2024-11-10 13:45:23 +0300

    Update ex13.c

--- ex13.c
+++ ex13.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
+#include <cs50.h>
+
+ int fact(int n){
+    if(n ==1 ||n== 0){
+        return 1;
+    } else {
+        return n*fact(n-1);
+    }
+ }
+
+int main(void){
+
+    int number;
+    do{
+        number = get_int("Please enter a number between 0 and 49 to calculate its factorial:");
+    }while(number<0 || number >50);
+
+     int result= fact(number);
+     printf("%d! = %d", result, fact(number));
+}
 
