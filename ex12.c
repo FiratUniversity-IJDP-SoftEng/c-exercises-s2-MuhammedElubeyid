20c2a8d8890d4aecbe4af7a5c0afb27a217d70d0
MuhammedElubeyid <eprrmkz12345@gmail.com>
2024-11-10 12:09:59 +0300

    Update ex12.c

--- ex12.c
+++ ex12.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void){
+      int a;
+       do{
+     a = get_int("Please enter a number between 0 and 49 to calculate its factorial:");
+       } while(a<0 || a>49);
+   unsigned long long result= 1;
+
+    for(int i=1; i<=a; i++){
+        result*=i;
+    }
+    printf("%d=! %llu",a,  result);
+}
 
