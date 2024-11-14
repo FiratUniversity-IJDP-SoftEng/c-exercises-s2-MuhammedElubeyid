6e26f59d2879e6389fd0df7ede24abcf1d90594e
MuhammedElubeyid <eprrmkz12345@gmail.com>
2024-11-14 21:28:10 +0300

    Update ex15.c

--- ex15.c
+++ ex15.c
@@ -1,1 +1,31 @@
+#include <stdio.h>
+
+int main (void){
+  for(int i= 1; i< 5;i++){
+    for(int j= i; j<= 5; j++){
+      printf("  ");
+    }
+    for(int j= 1; j<= i; j++){
+      printf("%d ", j);
+    }
+    for(int j= i-1; j>=1; j--){
+      printf("%d ", j);
+    }
+    printf("\n");
+  }
+
+  for(int i= 5; i>= 1;i--){
+    for(int j= i; j<= 5; j++){
+      printf("  ");
+    }
+    for(int j= 1; j<= i; j++){
+      printf("%d ", j);
+    }
+    for(int j= i-1; j>=1; j--){
+      printf("%d ", j);
+    }
+    printf("\n");
+  }
+
+}
 
